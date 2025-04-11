c2f1932b30979833400a51b3408b727dd0c8662a
Kayume <125704765+sunreek@users.noreply.github.com>
2025-04-10 21:11:32 -0300

    habilitar o swagger

--- site/streamlit_app.py
+++ site/streamlit_app.py
@@ -4,4 +4,16 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+empresa = st.selectbox("Empresa", ["PETR4", "VALE3", "MGLU3", "ABEV3"])
+volume = st.number_input("Quantidade de ações", min_value=1, step=1)
 
+precos = {
+    "PETR4": 34.70,
+    "VALE3": 67.20,
+    "MGLU3": 3.80,
+    "ABEV3": 15.45
+}
+
+if st.button("Calcular"):
+    total = volume * precos[empresa]
+    st.write(f"💰 Valor estimado: **R$ {total:,.2f}**")
